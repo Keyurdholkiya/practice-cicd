ea5359fd3ed7db4ab4506c6b7a1607577ad78727
Keyurdholkiya <keyurdholkiya2005@gmail.com>
2026-07-05 15:04:19 +0530

    add files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node.js 20 🚀");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "OK",
+        node: process.version
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
