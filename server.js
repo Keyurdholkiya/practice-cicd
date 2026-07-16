7ad08109c8188ea05cb5793adc135629b0a0eb74
Keyurdholkiya <keyurdholkiya2005@gmail.com>
2026-07-16 13:57:23 +0530

    changed health check to localhost and increased sleep

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.get('/', (req, res) => {
 
 // 🚀 HEALTH CHECK ENDPOINT (Auto-Rollback aur CI/CD ke liye)
 app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: "ERROR",
-        message: "Manually triggered failure for rollback test"
-    });
+   res.status(200).send('OK');
 });
 
 // Server start karne ke liye
